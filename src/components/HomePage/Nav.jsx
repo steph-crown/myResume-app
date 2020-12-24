b2e7b6832105ebca0b6cc6907fbd003000cd6913
Steph-crown <emmanuelstephen024@gmail.com>
2020-12-24 09:19:14 +0100

    I made a first commit of the Nav component. Created HomePage folder in component folder to store Homepage components and in css folder to store their styles

---
+++ src/components/HomePage/Nav.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './../../css/HomePage/Nav.css'
+
+const Nav = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <span className="my">my</span>
+                <span className="Resume">Resume</span>
+            </div>
+            <div className="links-at-center">
+                <a href="#">Home</a>
+                <a href="#">Get Started</a>
+                <a href="#">About</a>
+                <a href="#">Help</a>
+            </div>
+            <div className="links-at-end">
+                <a href="#">Sign Up</a>   
+                <a href="#" className="login">Log In</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Nav from './Nav'
 
 class index extends Component {
     render() {
         return (
             <div>
-                <h1>Hello world</h1>
+                <Nav />
             </div>
         );
     }
