55d984e4546a483a7d6e1624099db00cc32dde99
Steph-crown <emmanuelstephen024@gmail.com>
2020-12-31 10:23:20 +0100

    Created mobile menu with minimal styling

--- src/components/MobileMenuPage/index.jsx
+++ src/components/MobileMenuPage/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './index.css';
+import { Link } from "react-router-dom";
 
 const index = () => {
     return (
-        <div>
-            MOBILE MENU
+        <div className="mobile-menu">
+            <Link to="/" className="x">&times;</Link>
+            <Link to="/" className="a">Home</Link>
+            <Link to="/new" className="a">Get Started</Link>
+            <Link to="/about" className="a">About</Link>
+            <Link to="/help" className="a">Help</Link>
+            <Link to="/signup" className="a">Sign Up </Link>  
+            <Link to="/login" className="a">Log In</Link>
         </div>
     );
 }
