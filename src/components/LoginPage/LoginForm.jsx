109585618e7a5a0ee6645c4ac80512f765c4fb74
Steph-crown <emmanuelstephen024@gmail.com>
2021-01-03 20:27:33 +0100

    Almost done with login form

--- src/components/LoginPage/LoginForm.jsx
+++ src/components/LoginPage/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './../../css/Sign-Log/SignUpForm.css';
 import './../../css/Sign-Log/SignUpForm.css';
 import { ReactComponent as GoogleIcon } from './../Svgs/Google-SignIn.svg'
@@ -10,36 +10,45 @@ import * as ACTIONS from './../../store/actions/actions';
 import { connect } from "react-redux";
 
 
-const LoginForm = () => {
-    return (
-        <form action="">
-                <div className="email input">
-                    <input type="email" name="loginEmailAddress" id="email" placeholder="Email Address" value={this.props.email} onChange={this.handleChange} />
-                    <i className="fa fa-envelope input-icon"></i>
-                </div>
+class LoginForm extends Component {
+    handleChange = (event) => {
+        this.props.updateLoginInput(event.target.value, event.target.name)
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
 
-                <div className="password input">
-                    <input type="password" name="loginPassword" id="password" placeholder="Password" onChange={this.handleChange} value={this.props.password} />
-                    <i className="fa fa-eye-slash input-icon"></i>
-                </div>
-                <button>Log In</button>
-                <div className="line-or">
-                    <div className="line"></div>
-                    <p>Or Log In With</p>
-                    <div className="line"></div>
-                </div>
-                <div className="auth-buttons">
-                    <div className="google">
-                        <GoogleIcon className="icon" />
-                        <h5>Google</h5>
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                    <div className="email input">
+                        <input type="email" name="loginEmailAddress" id="email" placeholder="Email Address" value={this.props.email} onChange={this.handleChange} />
+                        <i className="fa fa-envelope input-icon"></i>
                     </div>
-                    <div className="facebook">
-                        <FacebookIcon className="icon" />
-                        <h5>Facebook</h5>
+    
+                    <div className="password input">
+                        <input type="password" name="loginPassword" id="password" placeholder="Password" onChange={this.handleChange} value={this.props.password} />
+                        <i className="fa fa-eye-slash input-icon"></i>
                     </div>
-                </div>
-            </form>
-    );
+                    <button>Log In</button>
+                    <div className="line-or">
+                        <div className="line"></div>
+                        <p>Or Log In With</p>
+                        <div className="line"></div>
+                    </div>
+                    <div className="auth-buttons">
+                        <div className="google">
+                            <GoogleIcon className="icon" />
+                            <h5>Google</h5>
+                        </div>
+                        <div className="facebook">
+                            <FacebookIcon className="icon" />
+                            <h5>Facebook</h5>
+                        </div>
+                    </div>
+                </form>
+        );
+    }
 }
 
 // Maps the redux state to props object
