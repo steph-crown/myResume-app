e32883a2aa7de3bd8666c55a93ae57a375d57732
Steph-crown <emmanuelstephen024@gmail.com>
2021-01-19 18:34:50 +0100

    still on dashboard menu

--- src/components/Dashboard/DashboardMenu.jsx
+++ src/components/Dashboard/DashboardMenu.jsx
@@ -11,18 +11,33 @@ const DashboardMenu = (props) => {
                 <span className="Resume">Resume</span>
             </Link>
             <div className={"nav-text " + (props.active==="profiles"?"is-active":"")}>
+                <div className="dash-icon">
+                    <i className="fa fa-user"></i>
+                </div>
                 <Link to="/dashboard"><h6>Profiles</h6></Link>
             </div>
             <div className={"nav-text "+(props.active==="downloads"?"is-active":"")}>
+                <div className="dash-icon">
+                    <i className="fa fa-home"></i>
+                </div>
                 <Link to="/downloads"><h6>Downloads</h6></Link>
             </div>
             <div className="nav-text sect">
+                <div className="dash-icon">
+                    <i className="fa fa-home"></i>
+                </div>
                 <Link to="/"><h6>Home</h6></Link>
             </div>
             <div className="nav-text sect">
+                <div className="dash-icon">
+                    <i className="fa fa-home"></i>
+                </div>
                 <Link to="/"><h6>Log Out</h6></Link>
             </div>
             <div className="nav-text sect">
+                <div className="dash-icon">
+                    <i className="fa fa-home"></i>
+                </div>
                 <Link to=""><h6>Help</h6></Link>
             </div>
         </div>
