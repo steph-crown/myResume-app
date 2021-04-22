3f28a0b7604a11fd0695aba22aeec1f01a373a8b
Steph-crown <emmanuelstephen024@gmail.com>
2021-04-22 18:23:58 +0100

    Working on forms, Making progress

--- src/components/ProfileForm/index.jsx
+++ src/components/ProfileForm/index.jsx
@@ -83,11 +83,13 @@ export default class ProfileForm extends Component {
                         </div>
                     </div>
 
-                    <div className="form-nav-sm">
-                        <svg onClick={goBack} viewBox="0 0 24 24" class="r-13gxpu9 r-4qtqp9 r-yyyyoo r-1q142lx r-50lct3 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1srniue"><g><path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path></g></svg>
+                    <svg onClick={goBack} viewBox="0 0 24 24" class="r-13gxpu9 r-4qtqp9 r-yyyyoo r-1q142lx r-50lct3 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1srniue"><g><path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path></g></svg>
 
+
+                    <div className="form-nav-sm">
+                        
                         <div className="pages-sm">
-                            <h5>{this.pagesStr[this.state.currPage]}</h5>
+                            <h5>{this.pagesStr[pageNo]}</h5>
                             <div className="pages-dot-container">
                                 {pagesDotsJSX}
                             </div>
@@ -98,7 +100,10 @@ export default class ProfileForm extends Component {
                         <CurrComp initialState={initialState} />
                         <div className="button-group">
                             <div className="left-group">
-                                <button className="next-button">Next: {this.pagesStr[pageNo + 1]}</button>
+                                <Link to={pathWithoutPage + (pageNo + 1)}>
+                                    <button className={"next-button " + (pageNo === this.pagesStr.length - 1 ? "no-display" : "")}>Next: {this.pagesStr[pageNo + 1]}</button>
+                                </Link>
+                                {console.log(this.pagesStr.length-1)}
                                 <button className="save-button">Save</button>
                             </div>
                             <div className="right-group">
