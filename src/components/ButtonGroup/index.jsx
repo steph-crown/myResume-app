41de634206bf8aca596e723344ab7f8071cbe036
Steph-crown <emmanuelstephen024@gmail.com>
2021-04-26 09:20:57 +0100

    Working on forms

--- src/components/ButtonGroup/index.jsx
+++ src/components/ButtonGroup/index.jsx
@@ -9,7 +9,7 @@ export default function ButtonGroup({pageNo, pagesStr, pathWithoutPage}) {
         <div className="button-group">
             <div className={"left-group " + (pageNo === pagesStr.length - 1 ? "no-next" : "")}>
                 <button type="submit" className="next-button">Next: {pagesStr[pageNo + 1]}</button>
-                <button type="button" className={"save-button "}>Save</button>
+                <button type={(pageNo === pagesStr.length - 1 ? "submit" : "button")} className={"save-button "}>Save</button>
             </div>
             <div className="right-group">
                 <button type="button" className="view-button">View CV</button>
