62b6b2f86167e5ca657637d4dfb0566d5687199a
Steph-crown <emmanuelstephen024@gmail.com>
2021-05-11 13:02:36 +0100

    Working on forms

--- src/components/ProfileForm/Extra.jsx
+++ src/components/ProfileForm/Extra.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Formik, Form } from "formik";
+import { Formik, Form, Field } from "formik";
 import ButtonGroup from './../ButtonGroup';
 import { useHistory } from "react-router-dom";
 import { CheckBox } from "./FieldList";
@@ -12,26 +12,62 @@ export default function Extra(props) {
     const [currentComponent, changeComponent] = useState(0);
 
     const initialValues = {
-        accomplishments: false,
-        languages: false,
-        affiliations: false,
-        interests: false,
-        certifications: false,
-        additionalInfo: false
+        boolVal: {
+            accomplishments: false,
+            languages: false,
+            affiliations: false,
+            interests: false,
+            certifications: false,
+            additionalInformation: false
+        },
+        textVal: {
+            accomplishments: "",
+            languages: "",
+            affiliations: "",
+            interests: "",
+            certifications: "",
+            additionalInformation: ""
+        },
+        labelValues: {
+            accomplishments: "Accomplishments",
+            languages: "Languages",
+            affiliations: "Affiliations",
+            interests: "Interests",
+            certifications: "Cerifications",
+            additionalInformation: "Additional Information"
+        }
     }
 
     const componentChanger = (values) => {
-        values = Object.entries(values).filter(x => x[1])
+        values = Object.entries(values.boolVal).filter(x => x[1])
         console.log(values);
-        values.length ?
-        changeComponent(1)
-        : changeComponent(2)
+        if (currentComponent === 0) {
+            if (values.length) changeComponent(1);
+            else changeComponent(2);
+        }
+        if (currentComponent === 1) {
+            changeComponent(2)
+        }
     }
 
     return (
         <div className="my-form">
-            <h3>Add additional sections to your resume</h3>
-            <p style={{color:"var(--green)"}}>These sections are optional.</p>
+            {
+                currentComponent < 2 && 
+                <>
+                    <h3>Add additional sections to your resume</h3>
+                    <p style={{color:"var(--green)"}}>These sections are optional.</p>
+                </>
+                
+            }
+
+            {
+                currentComponent === 2 &&
+                <>
+                    <h3>Your Resume is Ready!</h3>
+                </>
+            }
+            
             <Formik
                 initialValues={initialValues}
                 onSubmit={
@@ -46,32 +82,32 @@ export default function Extra(props) {
                         !currentComponent ?
                         <>
                             <div className="input-group f100">
-                                <CheckBox name="accomplishments">
+                                <CheckBox name="boolVal.accomplishments">
                                     Accomplishments
                                 </CheckBox>
                             </div>
                             <div className="input-group f100">
-                                <CheckBox name="languages">
+                                <CheckBox name="boolVal.languages">
                                     Languages
                                 </CheckBox>
                             </div>
                             <div className="input-group f100">
-                                <CheckBox name="affiliations">
+                                <CheckBox name="boolVal.affiliations">
                                     Affiliations
                                 </CheckBox>
                             </div>
                             <div className="input-group f100">
-                                <CheckBox name="interests">
+                                <CheckBox name="boolVal.interests">
                                     Interests
                                 </CheckBox>
                             </div>
                             <div className="input-group f100">
-                                <CheckBox name="certifications">
+                                <CheckBox name="boolVal.certifications">
                                     Certifications
                                 </CheckBox>
                             </div>
                             <div className="input-group f100">
-                                <CheckBox name="additionalInfo">
+                                <CheckBox name="boolVal.additionalInformation">
                                     Additional Information
                                 </CheckBox>
                             </div>
@@ -80,15 +116,16 @@ export default function Extra(props) {
                         : currentComponent === 1 ?
                         <>
                             {
-                                Object.entries(values).filter(x => x[1]).map(
+                                Object.entries(values.boolVal).filter(x => x[1]).map(
                                     arr => (
-                                        <div className="my-section">
-                                            <h2>{arr[0]}</h2>
-                                            
+                                        <div key={arr[0]} className="my-section">
+                                            <h5 style={{color: "var(--green", fontWeight: "300", marginTop: "32px"}}>{values.labelValues[arr[0]]}</h5>
+                                            <Field name={"textVal." + arr[0]} id={"textVal." + arr[0]} as="textarea" className="form-textarea" placeholder={"Enter Your " + values.labelValues[arr[0]]} />
                                         </div>
                                     )
                                 )
                             }
+                            <button type="button" className="next-button" onClick={()=>{componentChanger(values)}}>Next</button>
                         </>
                         : currentComponent === 2 ?
                         <>
