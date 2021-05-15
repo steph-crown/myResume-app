3b5a4a975219d44d5042546ceaeee6611036ed42
Steph-crown <emmanuelstephen024@gmail.com>
2021-05-15 02:04:58 +0100

    Working on pdf viewer

--- src/Templates/index01/index.jsx
+++ src/Templates/index01/index.jsx
@@ -1,123 +1,122 @@
-import React from 'react'
+import React from 'react';
+import './index.css'
 
 export default function index() {
     return (
-        <div>
-            <div class="cv-container">
-                <div class="cv">
-                    <header>
-                        <h1>EMMANUEL STEPHEN</h1>
-                        <h4>FRONTEND DEVELOPER</h4>
-                    </header>
-                    <main>
-                        <aside>
-                            <div class="group">
-                                <h5>PROFESSIONAL
-                                    <br /> SKILLS</h5>
-                                <p>HTML</p>
-                                <p>CSS</p>
-                                <p>Javascript</p>
-                                <p>React JS</p>
-                                <p>Figma</p>
-                                <p>Content Writing</p>
+        <div className="cv-container">
+            <div className="cv">
+                <header>
+                    <h1>EMMANUEL STEPHEN</h1>
+                    <h4>FRONTEND DEVELOPER</h4>
+                </header>
+                <main>
+                    <aside>
+                        <div className="group">
+                            <h5>PROFESSIONAL
+                                <br /> SKILLS</h5>
+                            <p>HTML</p>
+                            <p>CSS</p>
+                            <p>Javascript</p>
+                            <p>React JS</p>
+                            <p>Figma</p>
+                            <p>Content Writing</p>
+                        </div>
+                        <div className="group">
+                            <h5>PERSONAL SKILLS</h5>
+                            <p>Creative spirit</p>
+                            <p>Reliable and professional</p>
+                            <p>Organized</p>
+                            <p>Time management</p>
+                            <p>Team player</p>
+                            <p>Fast learner</p>
+                            <p>Motivated</p>
+                        </div>
+                        <div className="group">
+                            <h5>CONTACT</h5>
+                            <p>+234 909 019 4796
+                            </p>
+                            <p>emmanuelstephen024@gmail.com
+                            </p>
+                            <p>15 Oloruntosin, Mushin, Lagos, Nigeria</p>
+                        </div>
+                        <div className="group">
+                            <h5>LINKS</h5>
+                            <div className="sub-group">
+                                <h6>GITHUB</h6>
+                                <a href="https://github.com/steph-crown">https://github.com/steph-crown</a>
+
                             </div>
-                            <div class="group">
-                                <h5>PERSONAL SKILLS</h5>
-                                <p>Creative spirit</p>
-                                <p>Reliable and professional</p>
-                                <p>Organized</p>
-                                <p>Time management</p>
-                                <p>Team player</p>
-                                <p>Fast learner</p>
-                                <p>Motivated</p>
+                            <div className="sub-group">
+                                <h6>TWITTER</h6>
+                                <a href="https://twitter.com/stephcrown06">https://twitter.com/stephcrown06</a>
+
                             </div>
-                            <div class="group">
-                                <h5>CONTACT</h5>
-                                <p>+234 909 019 4796
+                            <div className="sub-group">
+                                <h6>LINKED IN</h6>
+                                <a href="https://www.linkedin.com/in/stephcrown">https://www.linkedin.com/in/stephcrown</a>
+
+                            </div>
+                            <div className="sub-group">
+                                <h6>PORTFOLIO</h6>
+                                <a href="https://steph-portfolio.vercel.app">https://steph-portfolio.vercel.app</a>
+
+                            </div>
+                        </div>
+                    </aside>
+                    <section className="main">
+                        <div className="main-group">
+                            <h5>PROFILE</h5>
+                            <p>Honest and hardworking MERN stack developer skilled at designing sites and writing clean code. Implement successful content based on user needs. Background includes graphics work, UI/UX design and performance optimisation</p>
+                        </div>
+                        <div className="line-container">
+                            <div className="line">
+                            </div>
+                        </div>
+                        
+                        <div className="main-group">
+                            <h5>EXPERIENCE</h5>
+                            <div className="sub-main-group">
+                                <h6>JavaScript Developer (Remote) </h6>
+                                <b>Space Universe | Sept 2020 - Jan 2021</b>
+                                <p>Developed, designed, tested and debugged applications from foundational level up to full production levels.
                                 </p>
-                                <p>emmanuelstephen024@gmail.com
+                                <p>Enhanced applications and improved performance through testing and validations against requirements and performance standards
                                 </p>
-                                <p>15 Oloruntosin, Mushin, Lagos, Nigeria</p>
                             </div>
-                            <div class="group">
-                                <h5>LINKS</h5>
-                                <div class="sub-group">
-                                    <h6>GITHUB</h6>
-                                    <a href="https://github.com/steph-crown">https://github.com/steph-crown</a>
+                            <div className="sub-main-group">
+                                <h6>FRONTEND Developer Intern </h6>
+                                <b>Migrant Solutions | Jun 2020 - Dec 2020</b>
+                                <p>Collaborated with a team of interns, supervised by a senior developer to design UI's and build frontend of web applications
+                                </p>
+                            </div>
+                        </div>
+                        <div className="line-container">
+                            <div className="line">
+                            </div>
+                        </div>
+                        <div className="main-group">
+                            <h5>EDUCATION</h5>
+                            <div className="sub-main-group">
+                                <h6>BACHELOR OF SCIENCE, MATHEMATICS</h6>
+                                <b>University of Lagos | 2019 - 2022</b>
+                            </div>
+                            <div className="sub-main-group">
+                                <h6>SSCE</h6>
+                                <b>Shalom Kliffon Secondary School  | 2012 - 2015</b>
+                            </div>
+                        </div>
+                        <div className="line-container">
+                            <div className="line">
+                            </div>
+                        </div>
+                        <div className="main-group">
+                            <h5>AWARDS</h5>
+                            <h6>BEST GRADUATING STUDENT</h6>
+                            <b>Shalom Kiliffon Secondary School | July 2015</b>
+                        </div>
 
-                                </div>
-                                <div class="sub-group">
-                                    <h6>TWITTER</h6>
-                                    <a href="https://twitter.com/stephcrown06">https://twitter.com/stephcrown06</a>
-
-                                </div>
-                                <div class="sub-group">
-                                    <h6>LINKED IN</h6>
-                                    <a href="https://www.linkedin.com/in/stephcrown">https://www.linkedin.com/in/stephcrown</a>
-
-                                </div>
-                                <div class="sub-group">
-                                    <h6>PORTFOLIO</h6>
-                                    <a href="https://steph-portfolio.vercel.app">https://steph-portfolio.vercel.app</a>
-
-                                </div>
-                            </div>
-                        </aside>
-                        <section class="main">
-                            <div class="main-group">
-                                <h5>PROFILE</h5>
-                                <p>Honest and hardworking MERN stack developer skilled at designing sites and writing clean code. Implement successful content based on user needs. Background includes graphics work, UI/UX design and performance optimisation</p>
-                            </div>
-                            <div class="line-container">
-                                <div class="line">
-                                </div>
-                            </div>
-                            
-                            <div class="main-group">
-                                <h5>EXPERIENCE</h5>
-                                <div class="sub-main-group">
-                                    <h6>JavaScript Developer (Remote) </h6>
-                                    <b>Space Universe | Sept 2020 - Jan 2021</b>
-                                    <p>Developed, designed, tested and debugged applications from foundational level up to full production levels.
-                                    </p>
-                                    <p>Enhanced applications and improved performance through testing and validations against requirements and performance standards
-                                    </p>
-                                </div>
-                                <div class="sub-main-group">
-                                    <h6>FRONTEND Developer Intern </h6>
-                                    <b>Migrant Solutions | Jun 2020 - Dec 2020</b>
-                                    <p>Collaborated with a team of interns, supervised by a senior developer to design UI's and build frontend of web applications
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="line-container">
-                                <div class="line">
-                                </div>
-                            </div>
-                            <div class="main-group">
-                                <h5>EDUCATION</h5>
-                                <div class="sub-main-group">
-                                    <h6>BACHELOR OF SCIENCE, MATHEMATICS</h6>
-                                    <b>University of Lagos | 2019 - 2022</b>
-                                </div>
-                                <div class="sub-main-group">
-                                    <h6>SSCE</h6>
-                                    <b>Shalom Kliffon Secondary School  | 2012 - 2015</b>
-                                </div>
-                            </div>
-                            <div class="line-container">
-                                <div class="line">
-                                </div>
-                            </div>
-                            <div class="main-group">
-                                <h5>AWARDS</h5>
-                                <h6>BEST GRADUATING STUDENT</h6>
-                                <b>Shalom Kiliffon Secondary School | July 2015</b>
-                            </div>
-
-                        </section>
-                    </main>
-                </div>
+                    </section>
+                </main>
             </div>
         </div>
     )
